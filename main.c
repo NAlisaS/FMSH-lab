5f51b759092c877e403f439a0fd60fb98879bdfe
Alisa <alisa4niki@mail.ru>
2024-10-06 14:53:26 +0300

    Update program in develop branch

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
-    printf("Hello, world!\n");
+    printf("Hello, my world!\n");
     return 0;
-=======
-#include <stdio.h>
-int main()
-{
-    printf("Hello, world!\n");
-    return 0;
->>>>>>> 38da65d67015e4a5a705eac629da7e040bb1f6fe
 }
